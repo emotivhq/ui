3a6204cc03503bdcd7a34518882610e456dc2b5e
Stuart Axel Owen <stuart@giftstarter.co>
2014-08-05 12:25:58 -0700

    Added analytics to product/retailer links

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -4,16 +4,18 @@
 
 
 GiftStarterApp.controller('HomeController', [
-            '$scope','Analytics',
-    function($scope,  Analytics) {
+            '$scope','Analytics','$window',
+    function($scope,  Analytics,  $window) {
         Analytics.track('client', 'loaded home');
 
-        $scope.retailerClicked = function(retailer) {
-            Analytics.track('client', 'reatiler clicked ' + retailer);
+        $scope.retailerClick = function(retailerUrl, retailerName) {
+            $window.open(retailerUrl, retailerName);
+            Analytics.track('client', 'reatiler clicked ' + retailerName);
         };
 
-        $scope.productClicked = function(product) {
-            Analytics.track('client', 'product clicked ' + retailer);
+        $scope.productClicked = function(productUrl, productName) {
+            $window.open(productUrl, productName);
+            Analytics.track('client', 'product clicked ' + productName);
         };
 
     }
