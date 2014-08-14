d05b4bfa3209eddf074259f62fc03dfa53809596
Stuart Axel Owen <stuart@giftstarter.co>
2014-08-14 11:01:35 -0700

    Made comments fade in/out

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -4,8 +4,8 @@
 
 
 GiftStarterApp.controller('HomeController', [
-            '$scope','Analytics','$window',
-    function($scope,  Analytics,  $window) {
+            '$scope','Analytics','$window','$http','$timeout',
+    function($scope,  Analytics,  $window,  $http,  $timeout) {
         Analytics.track('client', 'loaded home');
 
         $scope.retailerClick = function(retailerUrl, retailerName) {
@@ -18,5 +18,56 @@ GiftStarterApp.controller('HomeController', [
             Analytics.track('client', 'product clicked ' + productName);
         };
 
+        $scope.hotCampaigns = {};
+
+        $http({method: 'GET', url: '/giftstart/api/hot-campaigns?num_campaigns=3'})
+            .success(function(data) {
+                Analytics.track("client", "hot campaigns load succeeded");
+                $scope.hotCampaigns = data;
+
+                // TEST EST EST TEST TEST TEST
+                $scope.hotCampaigns.pitchins[0] = [{
+                    img: 'https://storage.googleapis.com/giftstarter-pictures/dev/u/f27213779.jpg',
+                    note: 'This is going to suite you so well!  You\'ve always been so awesome, this is me giving back.  Your besty loves you!!!!!', name: 'Stuart Owen'
+                },{
+                    img: 'https://storage.googleapis.com/giftstarter-pictures/u/f657795820.jpg',
+                    note: 'This is for your 30th.',
+                    name: 'Janet Phillip'
+                }];
+                // TEST EST EST TEST TEST TEST
+
+            }).error(function(data) {
+                Analytics.track("client", "hot campaigns load failed");
+            });
+
+        $scope.pitchinIndex = 0;
+        $scope.fadedIn = false;
+        function fadeInComment() {
+            $scope.fadedIn = true;
+            $scope.pitchinIndex += 1;
+            $timeout(commentDelay, 200);
+        }
+        function commentDelay() {
+            $timeout(fadeOutComment, 7000);
+        }
+        function fadeOutComment() {
+            $scope.fadedIn = false;
+            $timeout(fadeInComment, 200);
+        }
+        fadeInComment();
+        console.log($scope.fadedIn);
     }
 ]);
+
+GiftStarterApp.directive('gsHotCampaign', function() {
+    function link(scope, element, attrs) {
+        scope.goToUrl = function() {window.open('/giftstart?gs-id=' + scope.campaign.giftstart.gsid);};
+    }
+
+    return {
+        restrict: 'E',
+        scope: {campaign: '=', pitchins: '=', index: '=', fadeIn: '='},
+        link: link,
+        templateUrl: '/templates/angular/hot-campaign.html'
+    };
+});
