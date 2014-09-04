d0014d346016208b9c8b8861d34421d09e053826
Stuart Axel Owen <stuart@giftstarter.co>
2014-09-04 13:45:44 -0700

    Fixed direct link insert issue

--- product/product_api.py
+++ product/product_api.py
@@ -2,8 +2,8 @@ __author__ = 'stuart'
 
 import webapp2
 import json
-import fetch
-import tax
+import product_fetch
+import product_tax
 
 
 class ProductHandler(webapp2.RequestHandler):
@@ -11,15 +11,15 @@ class ProductHandler(webapp2.RequestHandler):
     def post(self):
         data = json.loads(self.request.body)
         if data['action'] == 'get':
-            self.response.write(json.dumps(fetch.product(data)))
+            self.response.write(json.dumps(product_fetch.product(data)))
 
         elif data['action'] == 'get-tax-and-shipping':
-            print(data)
             shipping_address = data['shipping_address']
             shipping_city = data['shipping_city']
             shipping_state = data['shipping_state']
             shipping_zip = data['shipping_zip']
-            tax_percent = tax.lookup(shipping_address, shipping_city, shipping_state, shipping_zip)
+            tax_percent = product_tax.lookup(shipping_address, shipping_city,
+                                             shipping_state, shipping_zip)
             self.response.write(json.dumps({'tax': tax_percent}))
 
 api = webapp2.WSGIApplication([('/product', ProductHandler)], debug=True)
