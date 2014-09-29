0a4fa6db5e24e28ca46681fc54fba1b6ee40f251
giftstarter-stuart <stuart@giftstarter.co>
2014-09-29 16:48:11 -0700

    Stop userlogin timestamps from being overwritten on put

--- gs_user/UserLogin.py
+++ gs_user/UserLogin.py
@@ -6,7 +6,7 @@ from google.appengine.ext import ndb
 class UserLogin(ndb.Model):
     uid = ndb.StringProperty(required=True)
     path = ndb.TextProperty(required=True)
-    timestamp = ndb.DateTimeProperty(auto_now=True)
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
 
     @staticmethod
     def register_login(uid, path):
