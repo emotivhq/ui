d97a0a1747ae3ee12b8461c9cee3bbfb60fad0ef
giftstarter-stuart <stuart@giftstarter.co>
2014-10-10 15:35:35 -0700

    All tests passing!

--- thank/test_gs_thank.py
+++ thank/test_gs_thank.py
@@ -1,6 +1,7 @@
 __author__ = 'Stuart'
 
 import unittest
+import skip32
 
 # UUTs
 import thank_core
@@ -27,8 +28,15 @@ class ThankCoreTestHandler(unittest.TestCase):
         """ The encode function should output a relatively even distribution
         of values - we want to avoid birthday attacks
         """
-        inputs = [str(i) for i in range(10000)]
-        self.assertEqual(True, False)
+        inputs = [thank_core.encode_secret(str(i)) for i in range(10000)]
+        values = [skip32.from_alpha(s) for s in inputs]
+        spread = [0] * 100
+        key_val = 2**32 / 100
+        for v in values:
+            spread[v // key_val] += 1
+        self.assertLess(max(spread) / float(min(spread)) - 1, 0.70,
+                        "Distribution should be within 70%, was " +
+                        str(spread))
 
     def test_decode_secret(self):
         """ Decoded secrets should match expected values
