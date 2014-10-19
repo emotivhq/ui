a1bcace8e208c1bc2bb588bb505cf54ab787e319
giftstarter-stuart <stuart@giftstarter.co>
2014-10-19 14:46:28 -0700

    Analytics now watches for view path changes

--- client/scripts/utilities/analytics.service.js
+++ client/scripts/utilities/analytics.service.js
@@ -1,13 +1,27 @@
 // Analytics Service
-GiftStarterApp.service("Analytics", ["$window","ABChoices", AnalyticsService]);
+GiftStarterApp.service("Analytics", ["$window","ABChoices","$rootScope",
+    "$location", AnalyticsService]);
 
-function AnalyticsService($window,  ABChoices) {
-    this.track = function(service, event, label, value) {
+function AnalyticsService($window,  ABChoices, $rootScope, $location) {
+
+    this.track = track;
+
+    this.track('client', 'loaded');
+
+    $rootScope.$on('$viewContentLoaded', pathChanged);
+
+    function pathChanged(event) {path($location.path())}
+
+    function path(path) {
+        if ($window.ga) {
+            $window.ga('send', 'pageview', {page: path});
+        }
+    }
+
+    function track(service, event, label, value) {
         if ($window.ga) {
             $window.ga('send', 'event', service, event,
                 ABChoices.getString(), value);
         }
-    };
-
-    this.track('client', 'loaded');
+    }
 }
