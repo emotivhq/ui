01cda026d27ddb2baff113ee29bfc46a73f4ecc0
giftstarter-stuart <stuart@giftstarter.co>
2014-10-29 00:57:37 -0700

    Fixed string prices being reported in button analytics

--- analytics.py
+++ analytics.py
@@ -79,10 +79,12 @@ class ButtonAnalyticsEvent(ndb.Model):
     def from_dict(d):
         def convert(name):
             s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-            s1['product_price'] = int(float(s1['product_price'])*100)
             return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
-        return ButtonAnalyticsEvent(**{convert(k): v for k, v in d.items()})
+        analytics_dict = {convert(k): v for k, v in d.items()}
+        analytics_dict['product_price'] = int(float(
+            analytics_dict['product_price'])*100)
+        return ButtonAnalyticsEvent(**analytics_dict)
 
 
 class ButtonAnalyticsHandler(webapp2.RequestHandler):
