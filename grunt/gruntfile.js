c6c67a211b2dd1280712080151df25437fa9a683
Jonathan Peck <peckjon@gmail.com>
2014-11-24 13:06:06 -0800

    concat task for Grunt

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -3,22 +3,22 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      build: {
-        src: '../client/scripts/app.config.js',
-        dest: '../client/scripts/app.config.min.js'
-      }
     concat: {
       build: {
         src: ['../client/scripts/app.config.min.js'],
         dest: '../client/scripts/giftstarter.min.js'
       }
     }
+    uglify: {
+      build: {
+        src: '../client/scripts/app.*.js',
+        dest: '../client/scripts/app.*.min.js'
+      }
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
