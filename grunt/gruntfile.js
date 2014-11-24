5ed0b3fce1d381a20c282935b873b6ae713d6ec3
Jonathan Peck <peckjon@gmail.com>
2014-11-24 12:45:39 -0800

    uglify task for Grunt

---
+++ grunt/gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+      },
+      build: {
+        src: '../client/scripts/app.config.js',
+        dest: '../client/scripts/app.config.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
