08580d2ab9bc59700e5641143b3d7d738d3b0a26
giftstarter-stuart <stuart@giftstarter.co>
2014-11-24 13:38:11 -0800

    Added grunt remove step

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -3,25 +3,37 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    remove: {
+      fileList: ['../client/scripts/out/*.js']
+    },
     concat: {
       build: {
-        src: ['../client/scripts/app.config.min.js'],
-        dest: '../client/scripts/giftstarter.min.js'
+        src: [
+          '../client/bower_components/angular/*.min.js',
+          '../client/bower_components/**/*.js',
+          '../client/scripts/utilities/ngAB/*.js',
+          '../client/scripts/app.module.js',
+          '../client/scripts/app.config.js',
+          '../client/scripts/app.run.js',
+          '../client/scripts/**/*.js'
+        ],
+        dest: '../client/scripts/out/app.js'
       }
-    }
+    },
     uglify: {
       build: {
-        src: '../client/scripts/app.*.js',
-        dest: '../client/scripts/app.*.min.js'
+        src: '../client/scripts/out/app.js',
+        dest: '../client/scripts/out/app.min.js'
       }
+    }
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-remove');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['remove', 'concat']);
 
 };
