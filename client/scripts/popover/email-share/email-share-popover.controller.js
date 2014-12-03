e6aa27bf8d6bc30996bc583943eb3ab57c931761
Hoon <hoonkong@gmail.com>
2014-12-03 00:25:08 -0800

    Bug fix for share email
    - Fixed the bug where extra “;” was causing invalid email: removed
    empty entries in the emails array
    - Converted the controller to module and organized the code

--- client/scripts/popover/email-share/email-share-popover.controller.js
+++ client/scripts/popover/email-share/email-share-popover.controller.js
@@ -2,76 +2,110 @@
  * Created by stuart on 9/2/14.
  */
 
+(function (app) {
+    'use strict';
 
-GiftStarterApp.controller('EmailSharePopoverController', [
-            '$scope','PopoverService','$http','UserService','Analytics',
-            'GiftStartService','$location', EmailSharePopoverController
-]);
+    var emailSharePopoverController = function ($scope,
+                                                PopoverService,
+                                                $http,
+                                                UserService,
+                                                Analytics,
+                                                GiftStartService,
+                                                $location) {
 
-function EmailSharePopoverController ($scope,  PopoverService,  $http,  UserService,  Analytics,
-         GiftStartService, $location) {
+        var email = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+\.([a-z0-9-]+)+$/i,
+            emails,
+            i;
 
-    $scope.toEmails = '';
-    $scope.fromEmail = UserService.email;
-    $scope.message = "Hey, check out this GiftStart, it's the bee's knees!\n\n" + UserService.name;
-    $scope.formValid = true;
-    $scope.emailUrl = "mailto:?subject=" +
-        encodeURI("Check out this awesome GiftStarter campaign!") + "&body=" +
-        encodeURI("Check out this awesome GiftStarter campaign, it's the " +
-            "bee's knees!\n\n" + $location.absUrl());
+        var trackEmailClientClick = function () {
+            Analytics.track('client', 'email client share clicked');
+        };
 
-    $scope.sending = false;
+        var sendEmail = function (to, from, message, share_url) {
+            Analytics.track('campaign', 'email share submitted');
+            $scope.sending = true;
+            $http({
+                method: 'PUT',
+                url: '/giftstart/share',
+                data: {
+                    to: to,
+                    from: from,
+                    message: message,
+                    share_url: share_url,
+                    gsid: GiftStartService.giftStart.gsid,
+                    sender_name: UserService.name,
+                    sender_uid: UserService.uid
+                }
+            })
+            .success(function() {
+                Analytics.track('campaign', 'email share succeeded');
+                $scope.sending = false;
+                $scope.toEmails = '';
+                $scope.fromEmail = '';
+                $scope.message = '';
+                $scope.hidePopover();
+            })
+            .error(function() {
+                $scope.sending = false;
+                Analytics.track('campaign', 'email share failed');
+            });
+        };
 
-    $scope.hidePopover = PopoverService.hidePopover;
+        $scope.toEmails = '';
+        $scope.fromEmail = UserService.email;
+        $scope.message = "Hey, check out this GiftStart, it's the bee's knees!\n\n" + UserService.name;
+        $scope.formValid = true;
+        $scope.emailUrl = "mailto:?subject=" +
+                          encodeURI("Check out this awesome GiftStarter campaign!") +
+                          "&body=" +
+                          encodeURI("Check out this awesome GiftStarter campaign, it's the bee's knees!\n\n" + $location.absUrl());
 
-    $scope.trackEmailClientClick = trackEmailClientClick;
+        $scope.sending = false;
 
-    var email = /^[a-z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-z0-9-]+\.([a-z0-9-]+)+$/i;
+        $scope.hidePopover = PopoverService.hidePopover;
 
-    $scope.submit = function() {
-        var emails = $scope.toEmails.replace(/[ \n]/g, "").split(/[,;]/);
-        $scope.formValid = emails.map(function(s){return email.test(s)})
-            .every(function(b){return b}) && email.test($scope.fromEmail);
+        $scope.trackEmailClientClick = trackEmailClientClick;
 
-        if ($scope.formValid) {
-            $location.search('re', btoa(JSON.stringify({
-                type: 'consumer',
-                uid: UserService.uid,
-                channel: 'email',
-                uuid: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-                    return v.toString(16);
-                })
-            })));
-            sendEmail(emails, $scope.fromEmail, $scope.message,
-                $location.absUrl());
-            $location.search('re', null);
-        }
+        $scope.submit = function () {
+            emails = $scope.toEmails.replace(/[ \n]/g, "").split(/[,;]/);
+
+            // Remove empty email entries in the array caused by extra delimiters
+            if (emails.length > 1) {
+                for (i = 0; i < emails.length; i++) {
+                    if (emails[i] === '') {
+                        emails.splice(i, 1);
+                        --i;
+                    }
+                }
+            }
+
+            $scope.formValid = emails.length > 0 && emails.every(function (s) { return email.test(s) }) && email.test($scope.fromEmail);
+
+            if ($scope.formValid) {
+                $location.search('re', btoa(JSON.stringify({
+                    type: 'consumer',
+                    uid: UserService.uid,
+                    channel: 'email',
+                    uuid: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                        return v.toString(16);
+                    })
+                })));
+                //sendEmail(emails, $scope.fromEmail, $scope.message, $location.absUrl());
+                $location.search('re', null);
+            }
+        };
     };
 
-    function sendEmail(to, from, message, share_url) {
-        Analytics.track('campaign', 'email share submitted');
-        $scope.sending = true;
-        $http({method: 'PUT', url: '/giftstart/share', data:{
-            to: to, from: from, message: message, share_url: share_url,
-            gsid: GiftStartService.giftStart.gsid,
-            sender_name: UserService.name, sender_uid: UserService.uid
-        }}).success(function() {
-            Analytics.track('campaign', 'email share succeeded');
-            $scope.sending = false;
+    app.controller('EmailSharePopoverController', [
+        '$scope',
+        'PopoverService',
+        '$http',
+        'UserService',
+        'Analytics',
+        'GiftStartService',
+        '$location',
+        emailSharePopoverController
+    ]);
 
-            $scope.toEmails = '';
-            $scope.fromEmail = '';
-            $scope.message = '';
-
-            $scope.hidePopover();
-        }).error(function() {
-            $scope.sending = false;
-            Analytics.track('campaign', 'email share failed');
-        });
-    }
-
-    function trackEmailClientClick() {
-        Analytics.track('client', 'email client share clicked');
-    }
-}
+}(GiftStarterApp));
