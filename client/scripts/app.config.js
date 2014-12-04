0f2ed2236edd1f9dd668d36ad6234e22b90338e0
giftstarter-stuart <stuart@giftstarter.co>
2014-12-03 20:52:46 -0800

    Basic content router controller added

--- client/scripts/app.config.js
+++ client/scripts/app.config.js
@@ -30,6 +30,11 @@ function appConfig($routeProvider,  $locationProvider,  $httpProvider) {
         {templateUrl: '/scripts/static-pages/what-is-it/what-is-it.html'})
         .when('/add-the-button',
         {templateUrl: '/scripts/static-pages/add-the-button/add-the-button.html'})
+        .when('/:path*', {
+            controller: 'ContentRouteController',
+            reloadOnSearch: false,
+            template: '<div ng-bind-html="{{ content }}"></div>'
+        })
         .otherwise({redirectTo: '/'});
 
     $locationProvider.hashPrefix('!').html5Mode({enabled: true});
@@ -39,4 +44,22 @@ function appConfig($routeProvider,  $locationProvider,  $httpProvider) {
 
 function facebookConfig(ezfbProvider, $httpProvider) {
     ezfbProvider.setInitParams({appId: window.fbAppId});
+}
+
+GiftStarterApp.controller('ContentRouteController', contentRouteController);
+
+function contentRouteController($scope, $routeParams, $http, $sce) {
+    $scope.templateUrl = '';
+    var baseUrl = '//content.giftstarter.co/';
+    function onRouteUpdate() {
+        $scope.templateUrl = baseUrl + $routeParams.path;
+    }
+    $scope.$on('$routeChangeSuccess', onRouteUpdate);
+    onRouteUpdate();
+
+    $http.get(baseUrl).then(function(response) {
+        $scope.content = $sce.trustAsHtml(response.data);
+    });
+
+
 }
