e47b3ac9eb9f4508cc74276a5b0b7ea254fb0561
Hoon <hoonkong@gmail.com>
2014-12-14 00:52:37 -0800

    GIFT-284: Mobile header nave menu
    - Added CSS to drop nav menu from the top
    - Now uses one consistent mark up for desktop and mobile

--- client/scripts/header/header.controller.js
+++ client/scripts/header/header.controller.js
@@ -4,71 +4,94 @@
  * Proprietary and confidential.
  */
 
-GiftStarterApp.controller('HeaderController', ['$scope','$location',
-    'UserService','Analytics','PopoverService', '$rootScope', '$interval',
-    HeaderController]);
+(function (app) {
 
-function HeaderController($scope,  $location,  UserService,  Analytics,
-                          PopoverService, $rootScope, $interval) {
-    var self = this;
-    this.thisRoute = $location.path().toString();
-    this.loggedIn = UserService.loggedIn;
-    this.mobile = device.mobile();
+    var HeaderController = function ($scope, $location, UserService, Analytics, PopoverService, $rootScope, $interval) {
+        var self = this;
+        this.thisRoute = $location.path().toString();
+        this.loggedIn = UserService.loggedIn;
+        this.mobile = device.mobile();
 
-    this.subliminalOffset = -3.0;
-    this.subliminalStyle = {'background-position-y': this.subliminalOffset +
-        'px'};
+        this.subliminalOffset = -3.0;
+        this.subliminalStyle = {'background-position-y': this.subliminalOffset + 'px'};
 
-    this.logout = logout;
-    this.login = login;
+        this.logout = logout;
+        this.login = login;
 
-    this.menuOpen = menuOpen;
+        this.menuOpen = menuOpen;
 
-    this.userImageUrl = UserService.profileImageUrl;
-    this.userProfileUrl = '/users/' + UserService.uid;
+        this.userImageUrl = UserService.profileImageUrl;
+        this.userProfileUrl = '/users/' + UserService.uid;
 
-    this.creating = $location.path().indexOf('/create') == 0;
+        this.creating = $location.path().indexOf('/create') === 0;
 
-    $interval(updateSubliminal, 3000);
+        $interval(updateSubliminal, 3000);
 
-    $scope.$on('login-success', updateLogin);
-    $scope.$on('logout-success', updateLogin);
-    $scope.$on('$routeChangeStart', routeChangeListener);
+        $scope.$on('login-success', updateLogin);
+        $scope.$on('logout-success', updateLogin);
+        $scope.$on('$routeChangeStart', routeChangeListener);
 
-    // for sizing using ng-class
-    function routeChangeListener(event, next) {
-        console.log($location.path());
-        self.creating = $location.path().indexOf('/create') == 0;
-        menuClose();
-        if (next.$$route) {
-            self.thisRoute = next.$$route.originalPath;
+        // for sizing using ng-class
+        function routeChangeListener(event, next) {
+            console.log($location.path());
+            self.creating = $location.path().indexOf('/create') === 0;
+            menuClose();
+            if (next.$$route) {
+                self.thisRoute = next.$$route.originalPath;
+            }
         }
-    }
 
-    function updateSubliminal() {
-        self.subliminalOffset -= 22.8178;
-        if (self.subliminalOffset < -253) {
-            self.subliminalOffset = -3;
+        function updateSubliminal() {
+            self.subliminalOffset -= 22.8178;
+            if (self.subliminalOffset < -253) {
+                self.subliminalOffset = -3;
+            }
+            self.subliminalStyle = {
+                'background-position-y': self.subliminalOffset + 'px'
+            };
         }
-        self.subliminalStyle = {
-            'background-position-y': self.subliminalOffset + 'px'
-        };
-    }
 
-    function login() {PopoverService.setPopover('login')}
+        function login() {PopoverService.setPopover('login')}
 
-    function logout() {
-        self.userImageUrl = '';
-        Analytics.track('user', 'logout from header');
-        UserService.logout();
-    }
+        function logout() {
+            self.userImageUrl = '';
+            Analytics.track('user', 'logout from header');
+            UserService.logout();
+        }
 
-    function updateLogin() {
-        self.loggedIn = UserService.loggedIn;
-        self.userImageUrl = UserService.profileImageUrl;
-        self.userProfileUrl = '/users/' + UserService.uid;
-    }
+        function updateLogin() {
+            self.loggedIn = UserService.loggedIn;
+            self.userImageUrl = UserService.profileImageUrl;
+            self.userProfileUrl = '/users/' + UserService.uid;
+        }
 
-    function menuOpen() {$rootScope.$broadcast('menu-open')}
-    function menuClose() {$rootScope.$broadcast('menu-close')}
-}
+        function menuOpen() {$rootScope.$broadcast('menu-open')}
+        function menuClose() {$rootScope.$broadcast('menu-close')}
+
+        var hideMenu = device.mobile();
+
+        $scope.actions = {
+            toggleMobileMenu: function () {
+                hideMenu = !hideMenu;
+
+                // Not recommended way: needs to happen in the directive
+                // TODO: future work item - move the header to a directive
+                hideMenu
+                    ? angular.element('ul.nav').removeClass('expanded')
+                    : angular.element('ul.nav').addClass('expanded');
+                //$scope.hideMenu = !$scope.hideMenu;
+            }
+        }
+    };
+
+    app.controller('HeaderController', [
+        '$scope',
+        '$location',
+        'UserService',
+        'Analytics',
+        'PopoverService',
+        '$rootScope',
+        '$interval',
+        HeaderController]);
+
+}(angular.module('GiftStarterApp')));
