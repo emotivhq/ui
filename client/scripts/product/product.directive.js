d5a092f0a9b1d9f0fed2c8cbc040a1bb90e1dfcd
Jon <jon@giftstarter.co>
2015-01-07 16:41:15 -0800

    GIFT-300 In search - clicking on Product Title should not open retailer link

--- client/scripts/product/product.directive.js
+++ client/scripts/product/product.directive.js
@@ -153,6 +153,7 @@ function gsProductSearch(ProductService, $location, Analytics, $window,
         scope.goToProduct = function($index, $event) {
             if (scope.selectedProduct == $index) {
                 //$window.open(scope.selectedProducts[$index].url, '_blank');
+                scope.startCampaignFrom($index);
                 $event.stopPropagation();
             }
         };
