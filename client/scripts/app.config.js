dd7858c364690711d4c4e15794c010108cc3e3f3
Jon <jon@giftstarter.co>
2015-01-21 18:00:27 -0800

    preparing for gift ideas page

--- client/scripts/app.config.js
+++ client/scripts/app.config.js
@@ -28,7 +28,7 @@ function appConfig($routeProvider,  $locationProvider,  $httpProvider) {
         {templateUrl: '/scripts/static-pages/concierge/concierge.html', reloadOnSearch: false})
         .when('/faq',
         {templateUrl: '/scripts/static-pages/faq/faq.html', reloadOnSearch: false})
-        .when('/giftideas',
+        .when('/:giftideas*',
         {templateUrl: '/scripts/static-pages/giftideas/giftideas.html', reloadOnSearch: false})
         .when('/partners',
         {templateUrl: '/scripts/static-pages/partners/partners.html', reloadOnSearch: false})
--- client/scripts/static-pages/giftideas/giftideas.controller.js
+++ client/scripts/static-pages/giftideas/giftideas.controller.js
@@ -4,22 +4,41 @@
  * Proprietary and confidential.
  */
 
-GiftStarterApp.controller('GiftideasController', ['$scope','$location','$timeout',
+GiftStarterApp.controller('GiftideasController', ['$scope','$http','$location','$timeout',
     GiftideasController]);
 
-function GiftideasController($scope,  $location,  $timeout) {
+function GiftideasController($scope, $http, $location,  $timeout) {
     $scope.location = $location;
+    $scope.showCategories = true;
 
-    $scope.scrollToSearch = function() {
-        if (Object.keys($location.search()).length) {
-            var selector = document.querySelector('#'+Object.keys($location.search())[0]);
-            var element = angular.element(selector);
-            element[0].scrollIntoView();
-        }
+    var path = $location.path();
+
+    //$scope.scrollToSearch = function() {
+    //    if (Object.keys($location.search()).length) {
+    //        var selector = document.querySelector('#'+Object.keys($location.search())[0]);
+    //        var element = angular.element(selector);
+    //        element[0].scrollIntoView();
+    //    }
+    //};
+
+    //$scope.$watch('location.search()', function() {
+    //    $timeout($scope.scrollToSearch, 400);
+    //    $timeout($scope.scrollToSearch, 700);
+    //});
+
+    this.populateCategories = function(path, $http) {
+        $http({method: 'GET', url: '/assets'+path+'.json'}).success(function (data) {
+            $scope.products = [];
+            $scope.categoryName = data.categoryName;
+            $scope.categoryBlurb = data.categoryBlurb;
+            angular.forEach(data.productList, function (value, key) {
+                $scope.showCategories = false;
+                console.log(value)
+                $scope.products.push(value);
+            });
+        });
     };
 
-    $scope.$watch('location.search()', function() {
-        $timeout($scope.scrollToSearch, 400);
-        $timeout($scope.scrollToSearch, 700);
-    });
+    this.populateCategories(path, $http);
+
 }
