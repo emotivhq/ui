3eaf2294a16a4d6680d569bcfa4c0082c3676114
Jon <jon@giftstarter.co>
2015-01-21 23:05:32 -0800

    gift ideas page

--- client/scripts/static-pages/giftideas/giftideas.controller.js
+++ client/scripts/static-pages/giftideas/giftideas.controller.js
@@ -10,8 +10,13 @@ GiftStarterApp.controller('GiftideasController', ['$scope','$http','$location','
 function GiftideasController($scope, $http, $location,  $timeout) {
     $scope.location = $location;
     $scope.showCategories = true;
+    $scope.showProducts = false;
+    $scope.showSingleProduct = false;
+    $scope.path = $location.path();
 
-    var path = $location.path();
+    var pathParts = $scope.path.split('/');
+    var category = pathParts.length>2?pathParts[2]:false;
+    var product = pathParts.length>3?pathParts[3]:false;
 
     //$scope.scrollToSearch = function() {
     //    if (Object.keys($location.search()).length) {
@@ -26,18 +31,32 @@ function GiftideasController($scope, $http, $location,  $timeout) {
     //    $timeout($scope.scrollToSearch, 700);
     //});
 
-    this.populateCategories = function(path, $http) {
-        $http({method: 'GET', url: '/assets'+path+'.json'}).success(function (data) {
-            $scope.products = [];
+    this.populateCategories = function(category, $http) {
+        $http({method: 'GET', url: '/assets/giftideas/'+category+'.json'}).success(function (data) {
+            $scope.groups = [];
             $scope.categoryName = data.categoryName;
+            $scope.categorySlug = data.categorySLug;
             $scope.categoryBlurb = data.categoryBlurb;
+            var prior=null;
             angular.forEach(data.productList, function (value, key) {
-                $scope.showCategories = false;
-                $scope.products.push(value);
+                if(prior!=null) {
+                    $scope.groups.push([prior,value]);
+                    prior=null;
+                } else {
+                    prior=value;
+                }
             });
+            if(prior!=null) {
+                $scope.groups.push([prior]);
+            }
         });
     };
 
-    this.populateCategories(path, $http);
+    if(category) {
+        $scope.showCategories = false;
+        $scope.showProducts = true;
+        $scope.showSingleProduct = false;
+        this.populateCategories(category, $http);
+    }
 
 }
