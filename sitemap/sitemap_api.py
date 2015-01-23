6e04110b710984aef88ab11be4e67d3518af275f
Jon <jon@giftstarter.co>
2015-01-22 18:50:50 -0800

    GIFT-486 Gift Ideas landing page

--- sitemap/sitemap_api.py
+++ sitemap/sitemap_api.py
@@ -18,7 +18,8 @@ class GiftideasHandler(webapp2.RequestHandler):
         app_url = config['app_url']#.replace("https://","http://")
         categories = ['baby','forhim']
 
-        head = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' \
+        head = '<?xml version="1.0" encoding="UTF-8"?>' \
+               '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' \
                ''
         body = ''
         for category_path in categories:
