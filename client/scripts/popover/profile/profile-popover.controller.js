f7751a7b4eeeb5f1a93f3d652e22c9c90166f75c
Jon <jon@giftstarter.co>
2015-02-04 17:40:20 -0800

    GIFT-441 upload pprofile picture

--- client/scripts/popover/profile/profile-popover.controller.js
+++ client/scripts/popover/profile/profile-popover.controller.js
@@ -10,9 +10,36 @@
     var profilePopoverController = function ($scope, UserService, PopoverService, GiftStartService, Analytics) {
 
         $scope.profilePicture = "http://localhost:8080/assets/about/img/jon.png";//UserService.profileImageUrl;
-        $scope.campaignPicture = UserService.profileImageUrl;
+        $scope.campaignPicture = $scope.profilePicture;
         $scope.useAsProfilePicture = false;
         $scope.editMode = false;
+
+        $scope.user = UserService.user;
+
+        var imageData;
+
+        $scope.imageSet = false;
+
+        $scope.imageUpdated = imageUpdated;
+        $scope.submit = submit;
+
+        function imageUpdated(data) {
+            $scope.imageSet = true;
+            imageData = data;
+        }
+
+        function submit() {
+            UserService.uploadProfileImage(imageData)
+                .success(function(newImageUrl) {
+                    //$scope.user.img_url = newImageUrl;
+                    $scope.campaignPicture = newImageUrl;
+                    $scope.editMode = false;
+                })
+                .error(function(reason) {
+                    console && console.log && console.log('Failed to update profile image', reason);
+                    $scope.editMode = false;
+                });
+        }
 
         $scope.cancel = function () {
             PopoverService.setPopover('note');
