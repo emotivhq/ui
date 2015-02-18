b59eedceeeaee116d7384d0db2cf27d6e14065be
Jon <jon@giftstarter.co>
2015-02-18 12:53:44 -0800

    GIFT-563 PyDoc (code-level function documentation)

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,17 +1,3 @@
-#pydoc -w ../abtest ../feeds ../giftstart ../gs_email ../gs_user ../gs_util ../login ../pay ../product ../sitemap ../social ../storage ../thank
+#requires epydoc: http://epydoc.sourceforge.net/manual-install.html
 cd $(dirname $0)
-pydoc -w ../
-rm _markerlib.*html
-rm cloudstorage.*html
-rm easy_install.*html
-rm lxml.*html
-rm manuel.*html
-rm mock.*html
-rm oauthlib.*html
-rm pkg_resources.*html
-rm requests.*html
-rm requests_oauthlib.*html
-rm setuptools.*html
-rm six.*html
-rm stripe.*html
-rm tl.*html
+epydoc ../analytics.py ../blog.py ../main.py ../render_app.py ../reports.py ../share.py ../test_gs_analytics.py ../test_gs_main.py ../abtest/ ../feeds/ ../giftstart/ ../gs_email/ ../gs_user/ ../gs_util/ ../login/ ../pay/ ../product/ ../sitemap/ ../social/ ../storage/ ../thank/
