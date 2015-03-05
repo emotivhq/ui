c30f8b1ff193abb7e9d3211ef76acce0cfb4eeba
Jon <jon@giftstarter.co>
2015-03-04 17:08:26 -0800

    GFTS-2 Build out initial project directory structure GFTS-3 Create (temporary) root subpath for new UI

---
+++ client/v2/js/controllers.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) GiftStarter, inc. - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+var GiftStarter2App = angular.module('GiftStarter2App', []);
+
+
+
