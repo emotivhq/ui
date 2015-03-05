9790f2565fe5f78deac4750b8cab7f1fa0ba35bc
Jon <jon@giftstarter.co>
2015-03-04 17:44:04 -0800

    GFTS-2 Build out initial project directory structure

--- client/v2/js/controllers.js
+++ client/v2/js/controllers.js
@@ -6,5 +6,10 @@
 
 var GiftStarter2App = angular.module('GiftStarter2App', []);
 
+GiftStarter2App.controller('HeadCtrl', function ($scope) {
+    //todo: consider http://plnkr.co/edit/0e7T6l
+    $scope.title = 'Welcome to GiftStarter!';
+});
+
 
 
