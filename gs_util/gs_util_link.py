e7e27871f7b78af450c43ed00aab65ac7178311a
Jon <jon@giftstarter.co>
2015-03-30 12:41:40 -0700

    GFTS-82 Encoding error while greating giftstart-it link

--- gs_util/gs_util_link.py
+++ gs_util/gs_util_link.py
@@ -3,6 +3,7 @@ __author__ = 'GiftStarter'
 
 import yaml
 import urllib
+import logging
 
 config = yaml.load(open('config.yaml'))
 
@@ -12,13 +13,26 @@ def make_giftstart_it_url(giftstart):
     @type giftstart: giftstart.GiftStart.GiftStart
     @rtype: str
     """
-    return '{app_url}/create?{query}'.format(**{
-        'app_url': config['app_url'],
-        'query': urllib.urlencode({
-            'product_url': giftstart.product_url,
-            'title': giftstart.product_title,
-            'price': giftstart.product_price,
-            'img_url': giftstart.product_img_url,
-            'source': 'GiftStarter Email',
-        }),
-    })
+    try:
+        return '{app_url}/create?{query}'.format(**{
+            'app_url': config['app_url'],
+            'query': urllib.urlencode({
+                'product_url': giftstart.product_url,
+                'title': giftstart.product_title,
+                'price': giftstart.product_price,
+                'img_url': giftstart.product_img_url,
+                'source': 'GiftStarter Email',
+            }),
+        })
+    except UnicodeEncodeError:
+        logging.warning("UnicodeEncodeError during make_giftstart_it_url for "+giftstart.jsonify())
+        return '{app_url}/create?{query}'.format(**{
+            'app_url': config['app_url'],
+            'query': urllib.urlencode({
+                'product_url': giftstart.product_url,
+                'title': giftstart.product_title.decode('utf-8', 'ignore'),
+                'price': giftstart.product_price.decode('utf-8', 'ignore'),
+                'img_url': giftstart.product_img_url,
+                'source': 'GiftStarter Email',
+            }),
+        })
