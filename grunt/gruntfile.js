137d7b9d998677f4c352f6ad7324a864852449f3
Jon <jon@giftstarter.co>
2015-04-09 11:33:07 -0700

    attempting to create sourcemaps for CSS

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -30,6 +30,10 @@ module.exports = function(grunt) {
         },
         concat: {
             build: {
+                options: {
+                    sourceMap: true,
+                    sourceMapStyle: "link"
+                },
                 src: [
                     '../client//bower_components/jquery/dist/jquery.min.js',
                     '../client//bower_components/jquery-ui/jquery-ui.min.js',
@@ -44,7 +48,7 @@ module.exports = function(grunt) {
                     '../client//bower_components/angular-resource/angular-resource.min.js',
                     '../client//bower_components/devicejs/lib/device.min.js',
                     '../client//bower_components/angular-ui-date/src/date.js',
-                    '../client//bower_components/bootstrap/dist/bootstrap.min..js',
+                    //'../client//bower_components/bootstrap/dist/js/bootstrap.min.js',
                     '../client//scripts/utilities/ng-ab/ng-ab.module.js',
                     '../client//scripts/utilities/ng-ab/ng-ab.service.js',
                     '../client//scripts/utilities/ng-ab/ng-ab.factory.js',
@@ -118,6 +122,11 @@ module.exports = function(grunt) {
                 dest: '../client/scripts/out/jquery.min.map'
             },
             css: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: "../client/stylesheets/compiled.css.map"
+                    //sourceMapStyle: "link"
+                },
                 src: ['../client/assets/bootstrap.css', '../client/scripts/out/css/**/*.css', '../client/bower_components/angucomplete/angucomplete.css', '../client/bower_components/jquery-ui/themes/smoothness/jquery-ui.css'],
                 dest: '../client/stylesheets/compiled.css'
             }
