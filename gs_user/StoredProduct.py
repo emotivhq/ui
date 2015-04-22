3549cb468b80e3ba5a0520d92fc38b76d599f2df
Jon <jon@giftstarter.co>
2015-04-22 14:34:35 -0700

    GFTS-150 Save Gift Idea aka "Save It For Later"

---
+++ gs_user/StoredProduct.py
@@ -0,0 +1,33 @@
+"""a Product which the User has saved for later use"""
+__author__ = 'GiftStarter'
+
+from google.appengine.ext import ndb
+from social.facebook import FacebookTokenSet
+from social.twitter import TwitterTokenSet
+from social.googleplus import GooglePlusTokenSet
+from login import EmailLoginPair
+import json
+
+
+class StoredProduct(ndb.Model):
+    """a Product which the User has saved for later use"""
+    timestamp = ndb.TimeProperty(auto_now_add=True)
+    uid = ndb.StringProperty(required=True)
+    url = ndb.StringProperty(required=True)
+    retailer = ndb.StringProperty(required=True)
+    price = ndb.IntegerProperty(required=True)
+    title = ndb.StringProperty(required=True)
+    description = ndb.TextProperty()
+    img = ndb.StringProperty(required=True)
+
+    def jsonify(self):
+        return json.dumps({
+                'timestamp': self.timestamp,
+                'uid': self.uid,
+                'url': self.url,
+                'retailer': self.retailer,
+                'price': self.price,
+                'title': self.title,
+                'description': self.description,
+                'img': self.img
+        })
