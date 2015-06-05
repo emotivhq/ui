34f1b66d3953d7c3f216785fe08c779ef3fb58c9
Jon <jon@giftstarter.co>
2015-06-05 13:36:32 -0700

    GFTS-417 FAQ page accordion implementation

--- client/scripts/shopify/button.js
+++ client/scripts/shopify/button.js
@@ -160,14 +160,16 @@
     }
 
     function makeData(action) {
+        var publicKey = '';
+        if (window.giftStartButton) {publicKey = window.giftStartButton.publicKey;}
         return {
             domain: window.location.host,
             path: window.location.pathname,
             uuid: makeUUID(),
-            productUrl: productUrl,
-            productTitle: title,
-            productPrice: price,
-            productImgUrl: imgUrl,
+            productUrl: document.location.href,
+            productTitle: self.product.title,
+            productPrice: self.product.price,
+            productImgUrl: self.product.imgUrl,
             publicKey: publicKey,
             scrollDepth: window.scrollY,
             screenW: window.screen.width,
@@ -200,6 +202,45 @@
             return v.toString(16);
         });
     }
+    var docCookies = {
+        getItem: function (sKey) {
+            if (!sKey) { return null; }
+            return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+        },
+        setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+            if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+            var sExpires = "";
+            if (vEnd) {
+                switch (vEnd.constructor) {
+                    case Number:
+                        sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+                        break;
+                    case String:
+                        sExpires = "; expires=" + vEnd;
+                        break;
+                    case Date:
+                        sExpires = "; expires=" + vEnd.toUTCString();
+                        break;
+                }
+            }
+            document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+            return true;
+        },
+        removeItem: function (sKey, sPath, sDomain) {
+            if (!this.hasItem(sKey)) { return false; }
+            document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+            return true;
+        },
+        hasItem: function (sKey) {
+            if (!sKey) { return false; }
+            return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+        },
+        keys: function () {
+            var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+            for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+            return aKeys;
+        }
+    };
 
     function getCookie() {
         var cookieVal = docCookies.getItem('gsButtonTrack');
@@ -212,13 +253,13 @@
 
     function getBorder() {
         return document.defaultView.getComputedStyle(
-            document.getElementById(gsButtonId),null)
+            document.querySelector('#gsbutton'),null)
             .getPropertyValue('border');
     }
 
     function getBackground() {
         return document.defaultView.getComputedStyle(
-            document.getElementById(gsButtonId),null)
+            document.querySelector('#gsbutton'),null)
             .getPropertyValue('background');
     }
 
