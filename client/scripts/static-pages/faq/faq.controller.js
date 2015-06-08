fabdcdea16270ce34745868a9f9161c1d7be65f7
theWingThing <thewingthing@gmail.com>
2015-06-08 14:51:15 -0700

    GFTS-417 FAQ page accordion implementation remade to use ng-repeat and items on controller side. added + and -

--- client/scripts/static-pages/faq/faq.controller.js
+++ client/scripts/static-pages/faq/faq.controller.js
@@ -12,6 +12,120 @@ function FaqController($scope,  $location,  $timeout) {
 
     $scope.openQuestions = [];
 
+    var question = function(question, answer) {
+        this.question = question;
+        this.answer = answer;
+    };
+
+    var section = function(name, questions) {
+        this.name = name;
+        this.questions = questions;
+    };
+
+    $scope.items = [
+        new section(
+            "OVERVIEW",
+            [
+                new question(
+                    "What is GiftStarter?",
+                    "GiftStarter is a Gifting Experience Service.  Our mission is to focus on the gifting experience, using technology as an enabler.  We enable the experience that captures giving between the recipient and those that give."
+                ),
+                new question(
+                    "How does it work?",
+                    "Search for and select a product using the search bar on the homepage, fill out the GiftStart information to share your story on why this is important to you or your gift recipient, and bring others along on the giving journey.  We let your family and friends choose their pieces of the gift to give, we send an awesome hand-crafted group card to remember the experience, and the gift too."
+                ),
+                new question(
+                    "Who can use the GiftStarter service?",
+                    "Anyone can use the GiftStarter service (who has a credit card). We are currently available in the continental United States."
+                ),
+                new question(
+                    "What methods of payment do you support?",
+                    "Visa, MasterCard, American Express, JCB, Discover, and Diners Club."
+                ),
+                new question(
+                    "How does GiftStarter compare to other crowdfunding sites?",
+                    "Other crowdfunding sites focus on enabling payment process. We focus on enabling the gifting experience all the way to delivering a hand-crafted group card and gift."
+                ),
+                new question(
+                    "Where do I go if I need help?",
+                    "We are always here to help!  Email us at giftconcierge@giftstarter.co.  We are also on standby on Twitter (@GiftStarter) and Facebook (www.facebook.com/giftstart).  Also, feel free to call us at 206-486-4849!"
+                )
+            ]
+        ),
+        new section(
+            "GIFTSTARTS",
+            [
+                new question(
+                    "How long will my Giftstart be live?",
+                    "From the time the GiftStart is created, it'll be live for 10 days."
+                ),
+                new question(
+                    "What happens if my GiftStart isn't completed before the due date?",
+                    "We send out a giftcard for the amount raised. Depending on the funding level, we will send the recipient a hand-crafted group card with the image of what was intended."
+                ),
+                new question(
+                    "How do I create a successful GiftStart?",
+                    "When you create the GiftStart title and description, share with others who will pitch in why it's awesome, why it is the perfect gift for your recipient, and any personal notes about the recipient. Encourage others who pitch in to share their personal stories related to the recipient too! After all, we're creating a gifting memory together!"
+                ),
+                new question(
+                    "When Am I Charged?",
+                    "We want to get your contribution to the gift as soon as possible, so we charge your card as soon as you pitch in."
+                )
+            ]
+        ),
+        new section(
+            "GIFTS",
+            [
+                new question(
+                    "After the GiftStart ends, when will the gift arrive?",
+                    "We will send both the hand-crafted group card (with all those that pitched in and their personal notes) and the gift within 3-5 business days of the GiftStart completion."
+                ),
+                new question(
+                    "How is the \"base price\" determined?",
+                    "The price of the gift at the time of campaign creation will be used to determine the \"base price.\""
+                ),
+                new question(
+                    "How do I know who pitched in on the gift?",
+                    "You'll receive a link to the GiftStart once you pitch in. If you're the recipient, you'll receive a hand-crafted gift card at the end of the campaign!"
+                )
+            ]
+        ),
+        new section(
+            "SERVICE FEES",
+            [
+                new question(
+                    "Does GiftStarter have a fee?",
+                    "Yes, our GiftStarter service fee is 8%."
+                ),
+                new question(
+                    "Who pays the fees?",
+                    "The 8% service fee is split up among the contributors to the GiftStarter campaign. For example, the fee would be $8.00 for a $100.00 item. And so if you have ten (10) people pitching in, then each person would only pay an additional $0.80. Yes... only 80 cents!! At that point, you could say it's almost free."
+                ),
+                new question(
+                    "Is there a sales tax?",
+                    "Yes, we have to pay all required legal taxes."
+                ),
+                new question(
+                    "Do I get a receipt?",
+                    "Yes, as soon as you pitch in, you will receive an email receipt."
+                )
+            ]
+        ),
+        new section(
+            "SECURITY",
+            [
+                new question(
+                    "Is my credit card information secure?",
+                    "Security is our top priority. To get specific, our site uses industry standard 128-bit SSL (Secure Sockets Layer) to ensure that sensitive data (ie. credit card number, name, address, etc.) is transmitted securely during every transaction. SSL encrypts the data before it is transmitted so that it can never be read. You will see the SSL seal displayed on the page during checkout and the address bar will change to begin with https."
+                ),
+                new question(
+                    "Is my personal information safe?",
+                    "We’re sticklers about this. We never share or sell any of the personal information of our registrants or gift-givers."
+                )
+            ]
+        )
+    ];
+
     $scope.scrollToSearch = function() {
         if (Object.keys($location.search()).length) {
             var selector = document.querySelector('#'+Object.keys($location.search())[0]);
