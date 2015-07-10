2122b6386dc54344ed727328c3d9da97c92fbe77
Jon <jon@giftstarter.co>
2015-07-10 11:45:40 -0700

    GFTS-563 Campaign create fails if logged in via LinkedIn

--- main.py
+++ main.py
@@ -49,8 +49,25 @@ class MainHandler(webapp2.RequestHandler):
                         self.redirect('/giftstart/' + gss[0].giftstart_url_title)
                         return
                 else:
-                    #bad login
+                    #bad login (usually, user hit Cancel during LinkedIn or Twitter dialog)
                     logging.error("Unable to find uid in request with staging_uuid {0} (likely they cancelled login): {1}".format(staging_uuid, self.request))
+                    try:
+                        gss = GiftStart.query(GiftStart.staging_uuid == staging_uuid).fetch(1)
+                        if not len(gss):
+                            time.sleep(5)
+                            gss = GiftStart.query(GiftStart.staging_uuid == staging_uuid).fetch(1)
+                        if len(gss):
+                            gs=gss[0]
+                            params = {'product_url': gs.product_url,
+                                      'title': gs.product_title,
+                                      'price': gs.product_price,
+                                      'img_url': gs.product_img_url,
+                                      'source': 'login cancellation redirect'}
+                            logging.error('REDIRECTING TO /create?'+urllib.urlencode(params))
+                            self.redirect('/create?'+urllib.urlencode(params))
+                            return
+                    except:
+                        pass
                     self.redirect('/')
 
         # Just render the app
