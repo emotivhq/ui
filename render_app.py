94f3635c05447758857744ffa2ddda945db37172
thinq4yourself <joel@thinq4yourself.com>
2015-09-12 16:49:22 +0000

    fixing #48306754473222 (Segment Identity tracking not working) & #46562616583950 (add GA identity tracking) & #49772334626094 (Insert Optimizely code at top of <head>).

--- render_app.py
+++ render_app.py
@@ -168,8 +168,9 @@ def remember_user(cookies, path):
                 js_insert = "window.loginDeets = ['{uid}', '{img_url}', " \
                             "'{token}', '{on_mailing_list}', '{name}', " \
                             "'{has_pitched_in}'];".format(**user_deets)
-                js_insert += "analytics.identify('{ uid }', {" \
-                            "name: '{ name }'," \
-                            "email: '{ token }'" \
-                            "});"
+                js_insert += "analytics.identify('{uid}', {{" \
+							 "name: '{name}'," \
+							 "email: '{token}'" \
+							 "}});".format(**user_deets)
+                js_insert += "ga('set', '&uid', {uid});".format(**user_deets)
     return js_insert
