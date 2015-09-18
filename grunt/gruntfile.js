5b0f47df9aebb2aa6ebb97b33d9a3d3899bfcefd
thinq4yourself <joel@thinq4yourself.com>
2015-09-18 00:27:55 +0000

    resolving issues with gruntified files (vendor, angular, app). Profile broken. Rebuild, moved script from out to webapp in scripts

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -45,7 +45,108 @@ module.exports = function(grunt) {
             }
         },
         concat: {
-            vendor: {
+			build: {
+                options: {
+                    sourceMap: createSourceMaps,
+                    sourceMapStyle: 'link'
+                },
+                src: [
+                    '../client/bower_components/jquery/dist/jquery.min.js',
+                    '../client/bower_components/jquery-ui/jquery-ui.min.js',
+                    '../client/bower_components/angular/angular.min.js',
+                    '../client/bower_components/angucomplete/angucomplete.js',
+                    '../client/bower_components/angular-route/angular-route.min.js',
+                    '../client/bower_components/angular-touch/angular-touch.min.js',
+                    '../client/bower_components/angular-easyfb/angular-easyfb.min.js',
+                    '../client/bower_components/angular-payments/lib/angular-payments.min.js',
+                    '../client/bower_components/angular-cookies/angular-cookies.min.js',
+                    '../client/bower_components/angular-sanitize/angular-sanitize.min.js',
+                    '../client/bower_components/angular-resource/angular-resource.min.js',
+                    '../client/bower_components/devicejs/lib/device.min.js',
+                    '../client/bower_components/angular-ui-date/src/date.js',
+                    '../client/scripts/utilities/ng-ab/ng-ab.module.js',
+                    '../client/scripts/utilities/ng-ab/ng-ab.service.js',
+                    '../client/scripts/utilities/ng-ab/ng-ab.factory.js',
+                    '../client/scripts/utilities/ng-ab/ng-ab.config.js',
+                    '../client/scripts/viewport-units-buggyfill.js',
+                    '../client/scripts/app.module.js',
+                    '../client/scripts/app.config.js',
+                    '../client/scripts/app.run.js',
+                    '../client/scripts/out/angular-template.js',
+                    '../client/scripts/frame/gs-view.directive.js',
+                    '../client/scripts/frame/gs-focus.directive.js',
+                    '../client/scripts/menu/menu.directive.js',
+                    '../client/scripts/home/scroll-play.directive.js',
+                    '../client/scripts/static-pages/about/about.controller.js',
+                    '../client/scripts/static-pages/faq/faq.controller.js',
+                    '../client/scripts/static-pages/giftideas/giftideas.controller.js',
+                    '../client/scripts/static-pages/concierge/concierge.controller.js',
+                    '../client/scripts/static-pages/howitworks/howitworks.controller.js',
+                    '../client/scripts/static-pages/oldbrowser/oldbrowser.controller.js',
+                    '../client/scripts/static-pages/partnerportal/partnerportal.controller.js',
+                    '../client/scripts/partnerportal/partnerportal.controller.js',
+                    '../client/scripts/static-pages/partners/partners.controller.js',
+                    '../client/scripts/static-pages/press/press.controller.js',
+                    '../client/scripts/static-pages/what-is-it/what-is-it.controller.js',
+                    '../client/scripts/home/home.js',
+                    '../client/scripts/giftsgivenbar/giftsgivenbar.controller.js',
+                    '../client/scripts/giftstart/thanks/thanked-campaigns.directive.js',
+                    '../client/scripts/giftstart/print/print.directive.js',
+                    '../client/scripts/header/header.controller.js',
+                    '../client/scripts/utilities/toast.service.js',
+                    '../client/scripts/utilities/toast.directive.js',
+                    '../client/scripts/utilities/analytics.service.js',
+                    '../client/scripts/utilities/head.controller.js',
+                    '../client/scripts/user/user.service.js',
+                    '../client/scripts/user/profile.controller.js',
+                    '../client/scripts/user/user_profile.controller.js',
+                    '../client/scripts/user/user_profile.directive.js',
+                    '../client/scripts/inspirationalexamples/inspirationalexamples.controller.js',
+                    '../client/scripts/login/facebook.service.js',
+                    '../client/scripts/login/twitter.service.js',
+                    '../client/scripts/login/googleplus.service.js',
+                    '../client/scripts/login/linkedin.service.js',
+                    '../client/scripts/login/email.service.js',
+                    '../client/scripts/login/login-or-create.controller.js',
+                    '../client/scripts/giftstart/giftstart.service.js',
+                    '../client/scripts/giftstart/giftstart.controller.js',
+                    '../client/scripts/giftstart/giftstart.directive.js',
+                    '../client/scripts/giftstart/thanks/thanks.directive.js',
+                    '../client/scripts/giftstart/create/giftstart-create.controller.js',
+                    '../client/scripts/share/share.controller.js',
+                    '../client/scripts/product/product.service.js',
+                    '../client/scripts/product/product.directive.js',
+                    '../client/scripts/giftstart/overlay/overlay.directive.js',
+                    '../client/scripts/popover/popover.service.js',
+                    '../client/scripts/popover/popover.directive.js',
+                    '../client/scripts/giftstart/funding-bar/funding-bar.directive.js',
+                    '../client/scripts/popover/login/login-popover.controller.js',
+                    '../client/scripts/popover/login/login-popover.directive.js',
+                    '../client/scripts/popover/note/note-popover.controller.js',
+                    '../client/scripts/popover/note/note-popover.directive.js',
+                    '../client/scripts/popover/profile/profile-popover.controller.js',
+                    '../client/scripts/popover/profile/profile-popover.directive.js',
+                    '../client/scripts/popover/pay/pay-popover.controller.js',
+                    '../client/scripts/popover/pay/pay-popover.directive.js',
+                    '../client/scripts/popover/pay/card.service.js',
+                    '../client/scripts/popover/thanks/thanks-popover.controller.js',
+                    '../client/scripts/popover/thanks/thanks-popover.directive.js',
+                    '../client/scripts/popover/email-share/email-share-popover.controller.js',
+                    '../client/scripts/popover/email-share/email-share-popover.directive.js',
+                    '../client/scripts/popover/sweepstakes/sweepstakes-popover.directive.js',
+                    '../client/scripts/staffpicks/staffpicks.controller.js',
+                    '../client/scripts/utilities/image-upload/image-upload.directive.js',
+                    '../client/scripts/utilities/local-storage.service.js',
+                    '../client/scripts/utilities/uservoice.js',
+                    '../client/scripts/utilities/ng-clamp.js',
+                    '../client/scripts/button/campaign-giftstart-it.directive.js',
+                    '../client/scripts/header/giftstart-it-header.directive.js',
+                    '../client/scripts/header/subscribe-header.directive.js',
+                    '../client/scripts/header/subscribe-header.directive.js'
+                ],
+                dest: '../client/scripts/out/app.js'
+            },
+			vendor: {
                 options: {
                     sourceMap: createSourceMaps,
                     sourceMapStyle: 'link'
@@ -64,7 +165,7 @@ module.exports = function(grunt) {
 					'../client/bower_components/flexslider/jquery.flexslider-min.js',
                     '../client/bower_components/OwlCarousel2/dist/owl.carousel.min.js'
 					 ],
-                dest: '../client/scripts/out/vendor.js'
+                dest: '../client/scripts/webapp/vendor.js'
             },
             angular: {
                 options: {
@@ -90,9 +191,9 @@ module.exports = function(grunt) {
 					'../client/bower_components/angular-flexslider/angular-flexslider.js',
 					'../client/bower_components/angular-boostrap/angular-bootstrap.min.js'
 					 ],
-                dest: '../client/scripts/out/angular.js'
+                dest: '../client/scripts/webapp/angular.js'
             },			
-			build: {
+			app: {
                 options: {
                     sourceMap: createSourceMaps,
                     sourceMapStyle: 'link'
@@ -179,10 +280,18 @@ module.exports = function(grunt) {
                     '../client/scripts/header/subscribe-header.directive.js',
                     '../client/scripts/header/subscribe-header.directive.js'
                 ],
-                dest: '../client/scripts/out/app.js'
+                dest: '../client/scripts/webapp/app.js'
             },
+			combine: {
+                options: {
+                    sourceMap: createSourceMaps,
+                    sourceMapStyle: 'link'
+                },
+                src: ['../client/scripts/webapp/vendor.js', '../client/scripts/webapp/angular.js', '../client/scripts/webapp/app.js'],
+				dest: '../client/scripts/webapp/core.js'
+			},
             build2: {
-                src: '../client//bower_components/jquery/dist/jquery.min.map',
+                src: '../client/bower_components/jquery/dist/jquery.min.map',
                 dest: '../client/scripts/out/jquery.min.map'
             },
             css: {
@@ -237,16 +346,20 @@ module.exports = function(grunt) {
                 compress: true
             },
             build: {
-                src: '../client/scripts/out/app.js',
-                dest: '../client/scripts/out/app.js'
+                src: '../client/scripts/webapp/app.js',
+                dest: '../client/scripts/webapp/app.js'
             },
             vendor: {
-                src: '../client/scripts/out/vendor.js',
+                src: '../client/scripts/webapp/vendor.js',
                 dest: '../client/scripts/webapp/vendor.js'
             },
             angular: {
-                src: '../client/scripts/out/angular.js',
+                src: '../client/scripts/webapp/angular.js',
                 dest: '../client/scripts/webapp/angular.js'
+            },
+            core: {
+                src: '../client/scripts/webapp/core.js',
+                dest: '../client/scripts/webapp/core.js'
             }
         },
 		cssmin: {
@@ -279,7 +392,6 @@ module.exports = function(grunt) {
                 globalReplace: false
             }
         }
-	
     });
 
     // Load the plugins
@@ -294,7 +406,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-bump');
 
     // Build tasks.
-    grunt.registerTask('default', ['remove', 'ngtemplates', 'sass:dist', 'sassy', 'concat', 'uglify']);	// the default task build the whole app
+    grunt.registerTask('default', ['remove', 'ngtemplates', 'sass:dist', 'sassy', 'concat']);	// the default task build the whole app
     grunt.registerTask('jshint', ['jshint']);							// Run jshint, catch errors, fix, code better
 	
 	// New SASS tasks, I call it sassy
