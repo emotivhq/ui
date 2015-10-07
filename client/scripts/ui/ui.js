f63bac897e726ef136d01956954db2c7a5a266c0
yaboi <tyler@tylergoelz.com>
2015-10-07 20:24:05 +0000

    Update #68. Update desktop version of logged in menu. Add logged in user drop down, rearranged item locations, updated search and explore drop down."

--- client/scripts/ui/ui.js
+++ client/scripts/ui/ui.js
@@ -1,48 +1,96 @@
 /**
-* Created with GiftStarter Platform-Upgrade.
-* User: thinq4yourself
-* Date: 2015-09-29
-* Time: 11:22 PM
-* To change this template use Tools | Templates.
-*/
-$(document)
-    .ready(function() {
-
-      // fix main menu to page on passing
-      $('.main.menu').visibility({
+ * Created with GiftStarter Platform-Upgrade.
+ * User: thinq4yourself
+ * Date: 2015-09-29
+ * Time: 11:22 PM
+ * To change this template use Tools | Templates.
+ */
+$(document).ready(function () {
+    // fix main menu to page on passing
+    $('.main.menu').visibility({
         type: 'fixed'
-      });
-      $('.overlay').visibility({
+    });
+    $('.overlay').visibility({
         type: 'fixed',
         offset: 80
-      });
-
-      // lazy load images
-      $('.load').visibility({
+    });
+    // lazy load images
+    $('.load').visibility({
         type: 'image',
         transition: 'vertical flip in',
         duration: 500
-      });
-
-      // show dropdown on hover
-      $('.main.menu  .ui.dropdown').dropdown({
+    });
+    // show dropdown on hover
+    $('.main.menu  .ui.dropdown').dropdown({
         on: 'click'
-      });
-	  
-	  // create mobile menu sidebar and attach to menu open
-      $('.ui.sidebar')
-        .sidebar('attach events', '.toc.item')
-      ;
-	
-	  // close message container
-	  $('.message .close')
-  		.on('click', function() {
-    		$(this)
-      			.closest('.message')
-      			.transition('fade')
-    		;
-  		})
-	  ;
-
-    })
-  ;
+    });
+    // create mobile menu sidebar and attach to menu open
+    $('.ui.sidebar').sidebar('attach events', '.toc.item');
+    // close message container
+    $('.message .close').on('click', function () {
+        $(this).closest('.message').transition('fade');
+    });
+    // search results logged in user menu
+    var content = [
+        {
+            title: 'Apple'
+        },
+        {
+            title: 'Bose'
+        },
+        {
+            title: 'butter LONDON'
+        },
+        {
+            title: 'Cuisinart'
+        },
+        {
+            title: 'Dyson'
+        },
+        {
+            title: 'Fat Cork'
+        },
+        {
+            title: 'Frends'
+        },
+        {
+            title: 'iCPooch'
+        },
+        {
+            title: 'Samsung'
+        },
+        {
+            title: 'Sturtevant\'s Sports'
+        },
+        {
+            title: 'Baby Crib'
+        },
+        {
+            title: 'Bassinet'
+        },
+        {
+            title: 'Cradle'
+        },
+        {
+            title: 'Crib Bedding'
+        },
+        {
+            title: 'Diaper Bag'
+        },
+        {
+            title: 'High Chair'
+        },
+        {
+            title: 'Playard'
+        },
+        {
+            title: 'Rocking Chair'
+        },
+        {
+            title: 'Stroller'
+        }
+    ];
+    $('.ui.search').search({
+        source: content
+    });
+});
