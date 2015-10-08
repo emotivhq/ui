da56eb8cded0eb250c6d1aa4982334a156217d64
yaboi <tyler@tylergoelz.com>
2015-10-08 23:13:53 +0000

    Update #75. Rewrote and refactored the way the navbars are showing depending on state of user login and page location.

--- client/scripts/webapp/app.js
+++ client/scripts/webapp/app.js
@@ -4190,26 +4190,26 @@ angular.module('GiftStarterApp').run(['$templateCache', function($templateCache)
     "\n" +
     "  <div class=\"mission\">\n" +
     "    <h2 class=\"ui segment vertical top bottom medium\">Our Mission</h2>\n" +
-    "\t  <div class=\"font ui class container vertical top medium segment middle aligned center grid centered\">\n" +
-    "\t\t  <div class=\"items divided\">\n" +
+    "\t  <div class=\"font ui class container vertical top medium  middle aligned center grid centered\">\n" +
+    "\t\t  <div class=\"ui items divided\">\n" +
     "\t\t\t  \n" +
     "\t\t  \n" +
     "    <p class=\"item\">\n" +
     "\t\tTo enable people to make meaningful connections with others using the power of gifts.</p>\n" +
-    "      <ul class=\"item items\">\n" +
-    "\t  \t<li class=\"item\">Want to send a gift to someone?</li>\n" +
+    "      <ul class=\"ui item items\">\n" +
+    "\t  \t<li class=\"item\" style=\"margin-top: 1em;\">Want to send a gift to someone?</li>\n" +
     "      \t<li class=\"item\">Want to send a group gift to someone?</li>\n" +
     "      \t<li class=\"item\">Want to save up piece by piece for a gift?</li>\n" +
     "\t  </ul>\n" +
     "\t\t\n" +
     "\t<p class=\"item\">Your time is valuable. Use GiftStarter for all of your gifting and payment option needs.</p>\n" +
     "\n" +
-    "\t  \t\t<a href=\"/giftideas\" class=\"ui button fuild icon labeled\" id=\"gifting-button\">\n" +
+    "\t  </div>\n" +
+    "\t  <a href=\"/giftideas\" class=\"ui button fuild icon labeled \" id=\"gifting-button\">\n" +
     "\t\t\t  <i class=\"gift icon left\"></i> \n" +
     "\t\t\t  You can with GiftStarter\n" +
     "\t\t</a>  \n" +
     "\t  </div>\n" +
-    "</div>\n" +
     "  <div class=\"milestones\">\n" +
     "    <h2 class=\"ui segment vertical top bottom medium\">Our Milestones</h2>\n" +
     "    <table>\n" +
@@ -4336,7 +4336,7 @@ angular.module('GiftStarterApp').run(['$templateCache', function($templateCache)
     "      <p>\n" +
     "\t\t  <a href=\"/giftconcierge\" class=\"ui button fuild icon labeled\" id=\"gifting-button\">\n" +
     "\t\t\t  <i class=\"life ring icon right\"></i> \n" +
-    "\t\t\t  FOr Help: Email The Gift Concierge\n" +
+    "\t\t\t  For Help: Email The Gift Concierge\n" +
     "\t\t\t</a>\n" +
     "\t\t<br/>\n" +
     "\t  <b>For Press and Partners:</b> Email <a href=\"mailto:partner@giftstarter.com\">partner@giftstarter.com</a><br/><br/>\n" +
@@ -6930,74 +6930,53 @@ function gsPrintUrl($location, $http) {
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * Proprietary and confidential.
  */
-
 (function (app) {
-
     var HeaderController = function ($scope, $location, UserService, Analytics, PopoverService, $rootScope, $interval, $timeout, $window, $http, $anchorScroll) {
         var self = this;
-		jQuery('.discover')
-  .popup({
-    inline   : true,
-    hoverable: true,
-    position : 'bottom left',
-    delay: {
-      show: 300,
-      hide: 800
-    }
-  })
-;
+        jQuery('.discover').popup({
+            inline: true,
+            hoverable: true,
+            position: 'bottom left',
+            delay: {
+                show: 300,
+                hide: 800
+            }
+        });
         this.thisRoute = $location.path().toString();
         this.loggedIn = UserService.loggedIn;
         this.mobile = device.mobile() || device.tablet();
-		function isSlim() {
-        	return($location.path() === '/join') ? true : false;
-    	}
-		this.makeSlim = isSlim();
-		this.slimHeader = isSlim();
-		$scope.slimHeader = isSlim();
-
         this.subliminalOffset = -3.0;
-        this.subliminalStyle = {'background-position-y': this.subliminalOffset + 'px'};
-
+        this.subliminalStyle = {
+            'background-position-y': this.subliminalOffset + 'px'
+        };
         this.logout = logout;
         this.showReset = showReset;
         this.closeLogin = closeLogin;
         this.loginKeyPress = loginKeyPress;
-
         this.menuOpen = menuOpen;
-
         this.userImageUrl = UserService.profileImageUrl;
         this.userProfileUrl = '/users/' + UserService.uid;
         this.userName = (UserService.name).toUpperCase();
-
         this.creating = $location.path().indexOf('/create') === 0;
         this.portaling = $location.path().indexOf('/portal') === 0;
-
         $scope.isHeaderOnly = self.thisRoute == '/header';
         $scope.isProvidence = self.thisRoute == '/yourvillage';
-		$scope.slimHeader = isSlim();
-        
         $scope.search = false;
         $scope.menu = false;
         $scope.notifyOpen = false;
-
         $scope.numNotifications = 0;
         $scope.numNotificationsUnseen = 0;
         $scope.notifications = null;
         $scope.notificationHover = false;
-
         $scope.showLoginwrapper = false;
         $scope.showBlackout = false;
-
         $interval(updateSubliminal, 3000);
-
         $scope.$on('login-success', updateLogin);
         $scope.$on('logout-success', updateLogin);
         $scope.$on('$routeChangeStart', routeChangeListener);
         $scope.$on('profile-image-changed', updateLogin);
-
         //check notifications for user
-        checkNotifications = function() {
+        checkNotifications = function () {
             if(UserService.loggedIn) {
                 $http({
                     method: 'GET',
@@ -7006,9 +6985,9 @@ function gsPrintUrl($location, $http) {
                     $scope.notifications = response.notifications;
                     $scope.numNotifications = 0;
                     $scope.numNotificationsUnseen = 0;
-                    for (item in $scope.notifications) {
+                    for(item in $scope.notifications) {
                         $scope.numNotifications++;
-                        if (!$scope.notifications[parseInt(item)].seen) {
+                        if(!$scope.notifications[parseInt(item)].seen) {
                             $scope.numNotificationsUnseen++;
                         }
                     }
@@ -7017,46 +6996,43 @@ function gsPrintUrl($location, $http) {
                 });
             }
         };
-        $scope.pollNotifications = function(){
+        $scope.pollNotifications = function () {
             checkNotifications();
             $timeout($scope.pollNotifications, 3000);
         };
         $scope.pollNotifications();
-
-        self.notificationsHoverIn = function() {
+        self.notificationsHoverIn = function () {
             $scope.notificationHover = true;
             notificationsSeen();
         };
-
-        self.notificationsHoverOut = function() {
+        self.notificationsHoverOut = function () {
             $scope.notificationHover = false;
         };
 
         function notificationsSeen() {
             $http({
-                method: 'POST', url: '/users/notify/' + UserService.uid + '.json',
+                method: 'POST',
+                url: '/users/notify/' + UserService.uid + '.json',
                 data: {
                     set_seen: '*'
                 }
             })
         }
-
-        self.openNotifications = function() {
+        self.openNotifications = function () {
             $scope.notifyOpen = true;
             notificationsSeen();
             self.closeMobileMenu();
             $scope.showBlackout = true;
             $anchorScroll('notificationlist')
         };
-
-        self.closeNotifications = function() {
+        self.closeNotifications = function () {
             $scope.showBlackout = false;
             $scope.notifyOpen = false;
         };
-
-        self.notificationClick = function(item) {
+        self.notificationClick = function (item) {
             $http({
-                method: 'POST', url: '/users/notify/' + UserService.uid + '.json',
+                method: 'POST',
+                url: '/users/notify/' + UserService.uid + '.json',
                 data: {
                     set_acknowledged: '["' + item.id + '"]'
                 }
@@ -7064,32 +7040,50 @@ function gsPrintUrl($location, $http) {
             $scope.notifications.splice($scope.notifications.indexOf(item), 1);
             self.closeNotifications();
         };
-
         // for sizing using ng-class
+
+        function isSlimMenu() {
+            return($location.path() === '/join') ? true : false;
+        }
+
+        function isMainMenu() {
+            return(!UserService.loggedIn && !isSlimMenu()) ? true : false;
+        }
+
+        function isUserMenu() {
+            return(UserService.loggedIn) ? true : false;
+        }
+
+        function updateMenuType() {
+            $scope.slimMenu = isSlimMenu();
+            $scope.mainMenu = isMainMenu();
+            $scope.userMenu = isUserMenu();
+        }
+        updateMenuType();
+
         function routeChangeListener(event, next) {
+            updateMenuType();
             self.creating = $location.path().indexOf('/create') === 0;
             self.portaling = $location.path().indexOf('/portal') === 0;
             menuClose();
-            if (next.$$route) {
+            if(next.$$route) {
                 self.thisRoute = next.$$route.originalPath;
             }
         }
 
         function updateSubliminal() {
             self.subliminalOffset -= 22.8178;
-            if (self.subliminalOffset < -253) {
+            if(self.subliminalOffset < -253) {
                 self.subliminalOffset = -3;
             }
             self.subliminalStyle = {
                 'background-position-y': self.subliminalOffset + 'px'
             };
         }
-
-        self.toggleMobileMenu = function() {
+        self.toggleMobileMenu = function () {
             $scope.menu = !$scope.menu;
         };
-
-        self.closeMobileMenu = function() {
+        self.closeMobileMenu = function () {
             $scope.menu = false;
         };
 
@@ -7102,11 +7096,12 @@ function gsPrintUrl($location, $http) {
             $scope.showBlackout = true;
             $scope.showLoginwrapper = true;
         }
-
-        self.showLogin = function() {
+        self.showLogin = function () {
             revealLogin();
             $rootScope.$broadcast('loginbox-show-login');
-            $timeout(function() {$rootScope.$broadcast('loginbox-show-login');}, 200);
+            $timeout(function () {
+                $rootScope.$broadcast('loginbox-show-login');
+            }, 200);
         };
 
         function showReset() {
@@ -7119,6 +7114,7 @@ function gsPrintUrl($location, $http) {
             self.userImageUrl = '';
             Analytics.track('user', 'logout from header');
             UserService.logout();
+            updateMenuType();
         }
 
         function updateLogin() {
@@ -7126,27 +7122,28 @@ function gsPrintUrl($location, $http) {
             self.userImageUrl = UserService.profileImageUrl;
             self.userProfileUrl = '/users/' + UserService.uid;
             self.userName = (UserService.name).toUpperCase();
+            updateMenuType();
         }
 
         function loginKeyPress($event) {
-            if($event.keyCode == 27)
-              closeLogin();
+            if($event.keyCode == 27) closeLogin();
         }
 
-        function menuOpen() {$rootScope.$broadcast('menu-open')}
-        function menuClose() {$rootScope.$broadcast('menu-close')}
+        function menuOpen() {
+            $rootScope.$broadcast('menu-open')
+        }
 
+        function menuClose() {
+            $rootScope.$broadcast('menu-close')
+        }
         var hideMenu = device.mobile() || device.tablet();
-
         $scope.headerSearchTerm = '';
-
         $scope.actions = {
             toggleMobileMenu: function () {
                 hideMenu = !hideMenu;
-
                 // Not recommended way: needs to happen in the directive
                 // TODO: future work item - move the header to a directive
-                if (hideMenu) {
+                if(hideMenu) {
                     device.mobile() && olark('api.box.show');
                     angular.element('ul.headerNav').removeClass('expanded');
                 } else {
@@ -7155,7 +7152,7 @@ function gsPrintUrl($location, $http) {
                 }
             },
             menuItemClicked: function (isLoginItem) {
-                if (device.mobile() || device.tablet()) {
+                if(device.mobile() || device.tablet()) {
                     device.mobile() && !isLoginItem && olark('api.box.show');
                     angular.element('ul.headerNav').removeClass('expanded');
                     hideMenu = true;
@@ -7169,16 +7166,13 @@ function gsPrintUrl($location, $http) {
                 }, 100);
             }
         };
-
-        $rootScope.$on('header-show-login', function(){
+        $rootScope.$on('header-show-login', function () {
             self.showLogin();
         });
-
-        $rootScope.$on('header-close-login', function(){
+        $rootScope.$on('header-close-login', function () {
             self.closeLogin();
         });
-
-        $rootScope.$on('password-reset-requested', function() {
+        $rootScope.$on('password-reset-requested', function () {
             if(self.loggedIn) {
                 self.logout();
                 window.location.reload();
@@ -7187,12 +7181,10 @@ function gsPrintUrl($location, $http) {
             }
             self.showReset();
         });
-
-        $scope.scrollTo = function(id) {
+        $scope.scrollTo = function (id) {
             $location.hash(id);
             $anchorScroll();
         };
-
         if($location.hash() == "nav_mobile") {
             $scope.menu = true;
         } else if($location.hash() == "searchbar") {
@@ -7200,37 +7192,33 @@ function gsPrintUrl($location, $http) {
         } else if($location.hash() == "nav_login") {
             self.showLogin();
         } else if($location.hash() == "nav_help" || $location.hash() == "nav_start") {
-            var menuopenlistener = function() {
+            var menuopenlistener = function () {
                 jQuery('#' + $location.hash()).removeClass("hover");
                 angular.element($window).off('mousemove', menuopenlistener);
             };
             jQuery('#' + $location.hash()).addClass("hover");
             angular.element($window).on('mousemove', menuopenlistener);
         }
-
         var producturl = decodeURIComponent($location.search().producturl);
-        if(producturl&&producturl!=""&&producturl!="true"&&producturl!="undefined") {
+        if(producturl && producturl != "" && producturl != "true" && producturl != "undefined") {
             var parser = document.createElement('a');
             parser.href = producturl;
             olark('api.box.expand');
             olark('api.chat.sendMessageToVisitor', {
-                body: "Welcome!  Can I help you gift this product from "+(parser.hostname=="localhost"?"another site":parser.hostname)+"?"
+                body: "Welcome!  Can I help you gift this product from " + (parser.hostname == "localhost" ? "another site" : parser.hostname) + "?"
             });
         }
-
         var userAgent = navigator.userAgent.toLowerCase();
-         if (userAgent .indexOf('safari')!=-1){
-           if(userAgent .indexOf('chrome')  > -1){
-             //browser is chrome
-           }else if((userAgent .indexOf('opera')  > -1)||(userAgent .indexOf('opr')  > -1)){
-             //browser is opera
-           }else{
-               jQuery('.menu-login > .submenu').css('margin-top', '-7px');
-           }
-          }
-
+        if(userAgent.indexOf('safari') != -1) {
+            if(userAgent.indexOf('chrome') > -1) {
+                //browser is chrome
+            } else if((userAgent.indexOf('opera') > -1) || (userAgent.indexOf('opr') > -1)) {
+                //browser is opera
+            } else {
+                jQuery('.menu-login > .submenu').css('margin-top', '-7px');
+            }
+        }
     };
-
     app.controller('HeaderController', [
         '$scope',
         '$location',
@@ -7243,26 +7231,24 @@ function gsPrintUrl($location, $http) {
         '$window',
         '$http',
         '$anchorScroll',
-        HeaderController])
-    .run(function($rootScope, $location, $anchorScroll, $routeParams) {
-		function isSlim() {
-        	return($location.path() === '/join') ? true : false;
-    	}
-		this.makeSlim = isSlim();
-		$rootScope.slimHeader = isSlim();
-		$rootScope.greybg = false;
-      //when the route is changed scroll to the proper element.
-      $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
-        $location.hash($routeParams.scrollTo);
-        $anchorScroll();
-		this.makeSlim = isSlim();
-		$rootScope.slimHeader = isSlim();
-		$rootScope.greybg = false;
-      });
-    })
-
+        HeaderController
+    ]).run(function ($rootScope, $location, $anchorScroll, $routeParams) {
+        //function isSlim() {
+        //    return($location.path() === '/join') ? true : false;
+        //}
+        //this.makeSlim = isSlim();
+        //$rootScope.slimHeader = isSlim();
+        $rootScope.greybg = false;
+        //when the route is changed scroll to the proper element.
+        $rootScope.$on('$routeChangeSuccess', function (newRoute, oldRoute) {
+            $location.hash($routeParams.scrollTo);
+            $anchorScroll();
+            //this.makeSlim = isSlim();
+            //$rootScope.slimHeader = isSlim();
+            $rootScope.greybg = false;
+        });
+    });
 }(angular.module('GiftStarterApp')));
-
 /**
  * Copyright (C) GiftStarter, inc. - All Rights Reserved.
  * Unauthorized copying of this file, via any medium is strictly prohibited.
