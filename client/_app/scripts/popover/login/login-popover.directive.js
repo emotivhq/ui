9df6056cd4c68d5be595f37bd62eb1437bb09eb0
thinq4yourself <joel@thinq4yourself.com>
2015-10-13 16:38:53 +0000

    removed _app & _public from git tracking in ./client app. Still working on it, supposed to be in another branch

--- client/_app/scripts/popover/login/login-popover.directive.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (C) GiftStarter, inc. - All Rights Reserved.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
-GiftStarterApp.directive('gsLoginPopover',
-    function() {return {restrict: 'E',
-        templateUrl: '/scripts/popover/login/login-popover.html'}});
--- client/_app/scripts/popover/thanks/thanks-popover.directive.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (C) GiftStarter, inc. - All Rights Reserved.
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
- */
-
-GiftStarterApp.directive('gsThanksPopover',
-    function() {return {restrict: 'E',
-        templateUrl: '/scripts/popover/thanks/thanks-popover.html'}});
