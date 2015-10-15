ed4777fdb536868d300557dedd44f08e10164260
yaboi <tyler@tylergoelz.com>
2015-10-15 20:43:59 +0000

    Fix #142 Add same functionality for 'invite friends' on last step as in the beginning before a user pitches in. Update disabled/styled state of 'pitch in' button and 'sign it' button. If invalid, they're disabled and grey; if valid, they're enabled and colored.

--- client/scripts/giftstart/giftstart.directive.js
+++ client/scripts/giftstart/giftstart.directive.js
@@ -3,20 +3,17 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited.
  * Proprietary and confidential.
  */
-
 GiftStarterApp.directive('gsCopyUrl', ['$window', gsCopyUrl]);
-
 
 function gsCopyUrl($window) {
     function link(scope, element, attrs) {
         element.on('click', function () {
-            if (!$window.getSelection().toString()) {
+            if(!$window.getSelection().toString()) {
                 // Required for mobile Safari
                 this.setSelectionRange(0, this.value.length)
             }
         });
     }
-
     return {
         restrict: 'A',
         link: link
