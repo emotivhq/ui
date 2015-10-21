1b0302654cb57bdb17d6fd64b881b0e914d9e8d4
yaboi <tyler@tylergoelz.com>
2015-10-21 22:50:42 +0000

    finish #117. Implement code in the routeChangeListener function to catch 'blog' and do a .location.href push to that path on an evelAsync watch. This allows the blog to load even though it's outside of our app.

--- client/scripts/header/header.controller.js
+++ client/scripts/header/header.controller.js
@@ -157,31 +157,14 @@
         }
         updateMenuType();
 
-        function routeChangeListener(event, next) {
+        function routeChangeListener(event, next, current) {
             /* jshint ignore:start */
-            //TODO: Tyler Goelz, 21-Oct-2015
-            //Implement better fix for /blog redirect
-            //  $rootScope.$on('$routeChangeStart', function (event, next, current) {
-            //      if(next && next.$$route && next.$$route.originalPath === '/login') {
-            //          // Stops the ngRoute to proceed
-            //          event.preventDefault();
-            //          // We have to do it async so that the route callback 
-            //          // can be cleanly completed first, so $timeout works too
-            //          $rootScope.$evalAsync(function () {
-            //              // next.$$route.redirectTo would equal be '/login'
-            //              $window.location.href = next.$$route.redirectTo;
-            //          });
-            //      } else if(next && next.$$route && next.$$route.originalPath === '/register') {
-            //          // Stops the ngRoute to proceed
-            //          event.preventDefault();
-            //          // We have to do it async so that the route callback 
-            //          // can be cleanly completed first, so $timeout works too
-            //          $rootScope.$evalAsync(function () {
-            //              // next.$$route.redirectTo would equal be '/login'
-            //              $window.location.href = next.$$route.redirectTo;
-            //          });
-            //      }
-            //  });
+            if(next && next.params && next.params.path === 'blog') {
+                event.preventDefault();
+                $rootScope.$evalAsync(function () {
+                    $window.location.href = next.params.path;
+                });
+            }
             /* jshint ignore:end */
             updateMenuType();
             self.creating = $location.path().indexOf('/create') === 0;
