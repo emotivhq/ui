2aba16588b5c0e79156844a3b6fe558fa62fe640
thinq4yourself <joel@thinq4yourself.com>
2015-12-19 00:43:58 +0000

    added gulp-bump, configuring

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var bump = require('gulp-bump');
+
+var getPackageJson = function () {
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+};
+
+// bump versions on package/bower/manifest
+gulp.task('bump', function () {
+  // reget package
+  var pkg = getPackageJson();
+  // increment version
+  var newVer = semver.inc(pkg.version, 'patch');
+
+  // uses gulp-filter
+  var manifestFilter = tasks.filter(['manifest.json']);
+  var regularJsons = tasks.filter(['!manifest.json']);
+
+  return gulp.src(['./bower.json', './package.json', './_Semantic/package.json', '_Angular/bower.json', '_Angular/angular.json'])
+    .pipe(tasks.bump({
+      version: newVer
+    }))
+    .pipe(manifestFilter)
+    .pipe(gulp.dest('./src'))
+    .pipe(manifestFilter.restore())
+    .pipe(regularJsons)
+    .pipe(gulp.dest('./'));
+});
+
+// Run the gulp tasks
+gulp.task('default', function(){
+  gulp.run('bump');
+});
